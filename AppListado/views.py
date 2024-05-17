504707a92c120a33668132ab9d65fa2513519d2c
Jesus Maria Villalobos <jveyes@gmail.com>
2024-05-17 09:11:50 -0500

    OK

--- AppListado/views.py
+++ AppListado/views.py
@@ -7,6 +7,7 @@ from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 def index(request):
     return render(request, 'index.html')
 
+
 # PAGINA --> db.html
 def db(request):
     query = request.GET.get('q')
