c4bc3e2b1a896623b9390557846858af3af3b9c0
Jesus Maria Villalobos <jveyes@gmail.com>
2024-05-17 23:38:57 -0500

    OK

--- RematesNacionales/settings.py
+++ RematesNacionales/settings.py
@@ -58,6 +58,8 @@ MIDDLEWARE = [
 
 CORS_ORIGIN_ALLOW_ALL = True
 
+CORS_ALLOW_CREDENTIALS = True
+
 CORS_ALLOWED_ORIGINS = [
 'https://rematesnacionales.datalink.consulting',
 'https://app.rematesnacionales.com',
