b6b975cae7dc2547dc071fdac638381f9920bed6
Jesus Maria Villalobos <jveyes@gmail.com>
2024-05-17 23:47:43 -0500

    OK

--- AppListado/apps.py
+++ AppListado/apps.py
@@ -4,7 +4,3 @@ from django.apps import AppConfig
 class ApplistadoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'AppListado'
-
-    def ready(self):
-        # Makes sure all signal handlers are connected
-        from AppListado import handlers  # noqa
