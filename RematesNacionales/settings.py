807c6bbf106b3b1ade78be271d555c7f644f2312
Jesus Maria Villalobos <jveyes@gmail.com>
2024-05-18 20:13:42 -0500

    OK

--- RematesNacionales/settings.py
+++ RematesNacionales/settings.py
@@ -63,6 +63,36 @@ CORS_ALLOWED_ORIGINS = [
     'https://rematesnacionales.datalink.consulting',
 ]
 
+CORS_ORIGIN_WHITELIST = (
+    'https://app.rematesnacionales.com',
+    'https://rematesnacionales.datalink.consulting',
+    'localhost:8000',
+    '127.0.0.1:8000'
+)
+
+CORS_ALLOW_METHODS = [
+'DELETE',
+'GET',
+'OPTIONS',
+'PATCH',
+'POST',
+'PUT',
+]
+
+CORS_ALLOW_HEADERS = [
+'accept',
+'accept-encoding',
+'authorization',
+'content-type',
+'dnt',
+'origin',
+'user-agent',
+'x-csrftoken',
+'x-requested-with',
+]
+
+
+
 INTERNAL_IPS = [																														# Agrega todo este bloque
 	'127.0.0.1',
 ]
