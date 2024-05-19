fd9ad7392625bf8e85645f1c966773c56ef67646
Jesus Maria Villalobos <jveyes@gmail.com>
2024-05-18 20:42:14 -0500

    OK

--- RematesNacionales/settings.py
+++ RematesNacionales/settings.py
@@ -55,14 +55,20 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware', 
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+#CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
     'https://rematesnacionales.datalink.consulting',
+    'https://app.rematesnacionales.com',
+    'http://localhost',
+    'http://127.0.0.1',
 ]
 
 CORS_ORIGIN_WHITELIST = (
     'https://rematesnacionales.datalink.consulting',
+    'https://app.rematesnacionales.com',
+    'http://localhost',
+    'http://127.0.0.1',
 )
 
 CORS_ALLOW_METHODS = [
