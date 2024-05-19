945601cf2a4e877dc32b9c99267159636f31b220
Jesus Maria Villalobos <jveyes@gmail.com>
2024-05-19 17:23:51 -0500

    OK

--- AppListado/urls.py
+++ AppListado/urls.py
@@ -5,5 +5,4 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('testdb/', views.testdb, name='testdb'),
     path('applistado/', views.applistado, name='applistado'),
-    #path('users/', views.user_list, name='user_list'),
 ]
